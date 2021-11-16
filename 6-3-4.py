588e23d045fe4ef7d0a84835f251716c0f5d71ac
Nolan Piselli <p22npiselli@fairfieldprep.org>
2021-11-15 22:41:44 -0500

    hw 6-3

--- 6-3-4.py
+++ 6-3-4.py
@@ -1,4 +1,13 @@
 # Author: Nolan (AMDG) 11/15/2021
  
 lst = list(input("Enter a list of numbers or letters seperated by spaces"))
+numbers = list(lst)
+length = len(numbers)
+mid = int(length - 1)
+end = int(length - 1)
+choice = input("Do you want the middle or ends of your input? ")
 
+if choice == "middle":
+    print("The middle of your input is" + str(numbers[1:mid]) + '.')
+if choice == "end":
+    print("The end of your input is" + str(numbers[1:end]) + '.')
